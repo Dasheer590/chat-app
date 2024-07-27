a9abea8323acd2dfb4ae407474e05df3c76fa44c
Dasheer590 <breezydanbaraka@gmail.com>
2024-07-27 02:55:23 +0200

    Include build directory for deployment

---
+++ client/build/precache-manifest.e053c4a4fe6ac3962de1ac126bd14763.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb7144c88079fa0df78f12dcf46b6e75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f2d2271fc0453571dd3",
+    "url": "/static/css/main.e2da20ab.chunk.css"
+  },
+  {
+    "revision": "6ecf4b63613a5eef50dc",
+    "url": "/static/js/2.a2863d4c.chunk.js"
+  },
+  {
+    "revision": "8f2d2271fc0453571dd3",
+    "url": "/static/js/main.7a4e4628.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  }
+]);
